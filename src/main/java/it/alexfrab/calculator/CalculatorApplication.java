5e00eabbdfe204451b8c958ae6743765e63e37b1
alexfrab <alessio.frabotta@gmail.com>
2018-10-16 14:45:05 +0200

    Update CalculatorApplication.java

--- src/main/java/it/alexfrab/calculator/CalculatorApplication.java
+++ src/main/java/it/alexfrab/calculator/CalculatorApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorApplication {
 
+	//questa volta modifico questo
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
